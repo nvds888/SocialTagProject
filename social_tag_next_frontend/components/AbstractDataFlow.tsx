2e84e0d1dd85d48f6a533bafa9fb45c552b571f1
Niels <nielsvdschepop12367@gmail.com>
2024-10-17 08:35:52 -0700

    abstractdata

--- social_tag_next_frontend/components/AbstractDataFlow.tsx
+++ social_tag_next_frontend/components/AbstractDataFlow.tsx
@@ -29,38 +29,39 @@ const AbstractDataFlow: React.FC = () => {
     }
 
     function drawDataFlow(time: number) {
-      if (ctx) {  // Add this null check
+      if (ctx && canvas) {  // Add canvas to the null check
         ctx.fillStyle = 'rgba(0, 20, 40, 0.1)'
         ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-      dataPoints.forEach((point, index) => {
-        ctx.beginPath()
-        ctx.arc(point.x, point.y, point.size, 0, Math.PI * 2)
-        ctx.fillStyle = `hsl(${(time * 0.05 + index * 10) % 360}, 100%, 50%)`
-        ctx.fill()
+        dataPoints.forEach((point, index) => {
+          ctx.beginPath()
+          ctx.arc(point.x, point.y, point.size, 0, Math.PI * 2)
+          ctx.fillStyle = `hsl(${(time * 0.05 + index * 10) % 360}, 100%, 50%)`
+          ctx.fill()
 
-        point.y += point.speed
-        point.x += Math.sin(time * 0.001 + index) * 0.5
+          point.y += point.speed
+          point.x += Math.sin(time * 0.001 + index) * 0.5
 
-        if (point.y > canvas.height) {
-          point.y = 0
-          point.x = Math.random() * canvas.width
-        }
-
-        // Connect nearby points
-        dataPoints.forEach((otherPoint, otherIndex) => {
-          if (index !== otherIndex) {
-            const distance = Math.hypot(point.x - otherPoint.x, point.y - otherPoint.y)
-            if (distance < 100) {
-              ctx.beginPath()
-              ctx.moveTo(point.x, point.y)
-              ctx.lineTo(otherPoint.x, otherPoint.y)
-              ctx.strokeStyle = `rgba(255, 255, 255, ${1 - distance / 100})`
-              ctx.stroke()
-            }
+          if (point.y > canvas.height) {
+            point.y = 0
+            point.x = Math.random() * canvas.width
           }
+
+          // Connect nearby points
+          dataPoints.forEach((otherPoint, otherIndex) => {
+            if (index !== otherIndex) {
+              const distance = Math.hypot(point.x - otherPoint.x, point.y - otherPoint.y)
+              if (distance < 100) {
+                ctx.beginPath()
+                ctx.moveTo(point.x, point.y)
+                ctx.lineTo(otherPoint.x, otherPoint.y)
+                ctx.strokeStyle = `rgba(255, 255, 255, ${1 - distance / 100})`
+                ctx.stroke()
+              }
+            }
+          })
         })
-      })
+      }
     }
 
     function animate(time: number) {
@@ -80,7 +81,7 @@ const AbstractDataFlow: React.FC = () => {
     return () => {
       window.removeEventListener('resize', handleResize)
     }
-  }}, [])
+  }, [])
 
   return <canvas ref={canvasRef} className="fixed inset-0 z-0" />
 }
