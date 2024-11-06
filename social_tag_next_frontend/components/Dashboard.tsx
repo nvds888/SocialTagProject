8b74836b5ef29c5db28df734141f0aa0ab424bbb
Niels <nielsvdschepop12367@gmail.com>
2024-11-06 07:40:15 -0800

    .

--- social_tag_next_frontend/components/Dashboard.tsx
+++ social_tag_next_frontend/components/Dashboard.tsx
@@ -377,7 +377,7 @@ const Dashboard: React.FC<Partial<{ username: string }>> = (props) => {
             ) : (
               <Popover>
                 <PopoverTrigger asChild>
-                  <button className="bg-[#40E0D0] text-black px-4 py-2 rounded-full hover:bg-[#40E0D0] transition-colors flex items-center">
+                  <button className="bg-white text-black px-4 py-2 rounded-none hover:bg-[#40E0D0] transition-colors flex items-center">
                     <Wallet size={18} className="mr-2" />
                     {connectedAccount.substring(0, 4)}...{connectedAccount.substring(connectedAccount.length - 4)}
                   </button>
