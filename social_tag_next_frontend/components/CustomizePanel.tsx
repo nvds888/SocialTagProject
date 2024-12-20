2734d7bdb2ba7e34accfe5d6c934d5f4669c6855
Niels <nielsvdschepop12367@gmail.com>
2024-12-20 05:02:22 -0800

    .

--- social_tag_next_frontend/components/CustomizePanel.tsx
+++ social_tag_next_frontend/components/CustomizePanel.tsx
@@ -430,12 +430,12 @@ const CustomizePanel: React.FC<CustomizePanelProps> = ({
         (asset: AlgorandAsset) => asset['asset-id']
       );
   
-      console.log('Making request to:', '/api/fetch-nft-metadata');
-      const metadataResponse = await axios.post(
-        '/api/fetch-nft-metadata',
-        { assetIds },
-        { withCredentials: true }
-      );
+      // In CustomizePanel.tsx
+const metadataResponse = await axios.post(
+  'https://www.social-tag.xyz/api/fetch-nft-metadata',
+  { assetIds },
+  { withCredentials: true }
+);
   
       // Import the NFT type from the modal or define it here
       interface NFTResponse {
