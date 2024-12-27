6b3a2f409ac54f691c6acf47a06a6c6697596d17
Niels <nielsvdschepop12367@gmail.com>
2024-12-27 02:52:07 -0800

    color

--- social_tag_next_frontend/components/CustomizePanel.tsx
+++ social_tag_next_frontend/components/CustomizePanel.tsx
@@ -1200,7 +1200,7 @@ if (data.nfd) {
         <Button
   onClick={() => handlePurchaseConfirmation('SOCIALS')}
   disabled={processingPaymentType !== null}
-  className="w-full bg-[#40E0D0] hover:bg-[#41e5d5] text-white"
+  className="w-full bg-[#40E0D0] hover:bg-[#48c7ba] text-white"
 >
   {processingPaymentType === 'SOCIALS' ? (
     <span className="flex items-center justify-center">
