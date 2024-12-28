033570a1069bda013f3f9eba0860a265ca3ee190
Niels <nielsvdschepop12367@gmail.com>
2024-12-28 12:42:29 -0800

    disable nft button

--- social_tag_next_frontend/components/CustomizePanel.tsx
+++ social_tag_next_frontend/components/CustomizePanel.tsx
@@ -906,8 +906,8 @@ if (data.nfd) {
   <div className="space-y-4">
   <div className="grid grid-cols-3 gap-4">
   <Button 
+  disabled
   onClick={() => setShowNFTModal(true)} 
-  disabled={!connectedWalletAddress}
   className={`w-full bg-[#FFB951] text-black px-4 py-2 rounded-lg flex items-center justify-center shadow-md hover:bg-[#FFB951]/90 transition-all border-2 border-black disabled:opacity-50 disabled:cursor-not-allowed`}
 >
   {isLoadingNFTs ? 'Loading...' : 'Select NFT'}
