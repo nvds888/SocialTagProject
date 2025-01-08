78f1671f739121206b5ff0fe92e683afd35ed865
Niels <nielsvdschepop12367@gmail.com>
2025-01-08 03:36:59 -0800

    server.js to include immersve wrapper

--- new-social-tag2/server.js
+++ new-social-tag2/server.js
@@ -10,6 +10,7 @@ const peraWalletRoutes = require('./peraWalletRoutes');
 const cors = require('cors');
 const themePurchaseRoutes = require('./themePurchaseRoutes');  
 const { initializeDistributionScheduler } = require('./token_distribution_wrapper');
+const { runRewardProcessor } = require('./immersveWrapper');
 
 
 
@@ -19,6 +20,8 @@ const PORT = process.env.PORT || 5000;
 
 // Initialize token distribution scheduler
 initializeDistributionScheduler();
+// Initialize Immersve reward processor
+runRewardProcessor();
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
